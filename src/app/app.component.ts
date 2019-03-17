1cb4bdad6a2f8694e520ccf8d840643497cf62b6
Richard Afro <richyafro@hotmail.com>
2019-03-17 09:20:32 +0100

    New properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'event-scheduler';
+  events: Event[] = [];
+
+  onNewEvent(event: Event) {
+    this.events = this.events.concat(event);
+  }
 }
