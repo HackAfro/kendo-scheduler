cba7a08b8502474b4b2225a8b679b8f55085d51f
Richard Afro <richyafro@hotmail.com>
2019-03-17 09:20:41 +0100

    Add new components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SchedulerModule } from '@progress/kendo-angular-scheduler';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
 
 import { AppComponent } from './app.component';
+import { EventFormComponent } from './event-form/event-form.component';
+import { SchedulerComponent } from './scheduler/scheduler.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventFormComponent,
+    SchedulerComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SchedulerModule,
+    BrowserAnimationsModule,
+    DateInputsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
